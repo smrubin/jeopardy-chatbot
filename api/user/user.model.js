f1e3d3ced083ee94334494c9317e1e714b59a4ea
smrubin <srubin@deloitte.com>
2017-07-03 15:04:31 -0400

    removing id field from model

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -2,10 +2,6 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
     username: DataTypes.STRING,
     score: {
       type: DataTypes.INTEGER,
